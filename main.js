6570db1af17334d9acfeb00a704bd5ccf9098e7e
ilija8897 <60179478+ilija8897@users.noreply.github.com>
2026-06-27 08:05:03 +0000

    add uppercase + digits + simbols + random

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 import { generatePassword } from './passwordGenerator.js'
 
-console.log(generatePassword(5))
-console.log(generatePassword(30))
+console.log(generatePassword(8, 1))
+console.log(generatePassword(12, 123))
+console.log(generatePassword(12, 123, { useSpecial: true }))
+console.log(generatePassword(8, 1, { useUppercase: false, useDigits: false }))
--- passwordGenerator.js
+++ passwordGenerator.js
@@ -1,14 +1,24 @@
-const LOWERCASE = 'abcdefghijklmnopqrstuvwxyz';
+const LOWERCASE = 'abcdefghijklmnopqrstuvwxyz'
+const UPPERCASE = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+const DIGITS = '0123456789'
+const SPECIAL = '!@#$%^&*'
 
-const generatePassword = (length, { useUppercase = true, useDigits = true }) => {
-    const alphabet = LOWERCASE
+const nextRandom = (number) => (16807 * number) % 2147483647;
+
+export const generatePassword = (length, seed, options = {}) => {
+    const { useUppercase = true, useDigits = true, useSpecial = false } = options;
+    let alphabet = LOWERCASE;
+
+    if (useUppercase) alphabet += UPPERCASE;
+    if (useDigits) alphabet += DIGITS;
+    if (useSpecial) alphabet += SPECIAL;
+    let current = seed
     let result = ""
     for(let i=0; i <= length-1; i++) {
-        const index = i % alphabet.length
+        current = nextRandom(current)
+        const index = current % alphabet.length
     
         result = result + alphabet[index]
     }
     return result
 }
-
-export { generatePassword };
